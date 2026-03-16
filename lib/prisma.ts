879ec4f77e9df67527447b7c89a29cc7fae1b8e7
Bernadette Hudlin <owner@dawahucollective.com>
2026-03-16 11:25:15 -0400

    Add Prisma PostgreSQL adapter for Prisma 7

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,25 @@
-// lib/prisma.ts
-
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set.");
+}
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
 export const prisma =
-  global.prisma ||
+  global.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error", "warn"],
   });
 
